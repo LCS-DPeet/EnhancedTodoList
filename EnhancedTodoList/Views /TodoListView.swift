d1c0c58e7e7e0d600b9dc335724935dba3c9e544
Danika Peet <DPeet@lcs.on.ca>
2024-11-12 11:55:13 -0500

    Added tab view.

---
+++ EnhancedTodoList/Views /TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  EnhancedTodoList
+//
+//  Created by Danika Peet on 2024-11-12.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text("Todo List View")
+    }
+}
+
+#Preview {
+    TodoListView()
+}
