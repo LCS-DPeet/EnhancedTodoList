5d3e414f9321107e637792f332cbe94899da0554
Danika Peet <DPeet@lcs.on.ca>
2024-11-12 11:38:41 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by Danika Peet on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingView()
+        }
+    }
+}
